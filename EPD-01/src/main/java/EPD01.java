ba090551537b4245fac74c3d01c289d6bb33277c
Sergio Izquierdo <sergioizq.dev@gmail.com>
2026-09-24 15:36:15 +0200

    Carga inicial

---
+++ EPD-01/src/main/java/EPD01.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author sergio
+ */
+public class EPD01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
